0eb9ef6ad4bd57d4d6aa3f121d2ce3e046089486
Michael Babker <michael.babker@gmail.com>
2019-06-29 14:17:07 -0500

    Missing import

--- src/Command/EditEntryCommand.php
+++ src/Command/EditEntryCommand.php
@@ -11,6 +11,7 @@ namespace Joomla\Keychain\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 /**
  * Command class to edit an entry in a keychain
